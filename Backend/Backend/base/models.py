1424e5ac0e3a018befd35e129755515c81dedafb
Minhdon <minhdoan0512@gmail.com>
2025-11-19 02:24:12 +0700

    first-commit

--- Backend/Backend/base/models.py
+++ Backend/Backend/base/models.py
@@ -9,7 +9,7 @@ class Item(models.Model):
     wareHouseID=models.CharField(max_length=20,default="0")
     cost=models.DecimalField(max_digits=10,decimal_places=0,default='0.00')
     status=models.BooleanField(default=True)
-    img=models.CharField(max_length=2000,default="no_images.img")
+    img=models.CharField(max_length=20000,default="no_images.img")
     productDesc=models.CharField(max_length=1000,default="mặc định")
 
 
